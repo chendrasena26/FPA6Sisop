f1563287a3e57cdc6b5eb08e0b09a5a441ccc879
chendrasena26 <chendrasena.oe@gmail.com>
2017-12-08 02:05:01 +0700

    rm custom

--- rm-custom.c
+++ rm-custom.c
@@ -2,7 +2,7 @@
 #include "stat.h"
 #include "user.h"
 
-int main(int argc, int *argv[])
+main(int argc, int *argv[])
 {
   if(argc<2) {
 	printf(2,"tak ada nama file..\n");
@@ -10,7 +10,7 @@ int main(int argc, int *argv[])
   }
   int i;
   for(i=1;i<argc;i++) {
-	stat=unlink(argv[i]);
+	int stat=unlink(argv[i]);
 	if(stat!=0) {
 	  printf(2, "gagal menghapus %s\n",argv[i]);
 	  exit();
