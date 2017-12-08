7e1dbdeeda454383117353bf768aba4a2daba288
mahendrabuyung <32903343+mahendrabuyung@users.noreply.github.com>
2017-12-08 19:41:50 +0700

    Update touch.c
    rdwr nya

--- touch.c
+++ touch.c
@@ -8,14 +8,14 @@ int main(int argc, char* argv[])
 
 	if(argc<2)
     {
-        printf(2, "Syntax Error, touch FILENAME\n");
+        printf(1, "Syntax Error, touch FILENAME\n");
         exit();
     }
 
     int i, file;
     for(i = 1; i<argc; i++)
     {
-        file = open(argv[i], O_CREATE);
+        file = open(argv[i], O_CREATE | O_RDWR);
         if(file < 0) 
         {
             printf(2, "Failed to create file %s\n", argv[i]);
